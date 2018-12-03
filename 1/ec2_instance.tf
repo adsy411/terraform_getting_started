83324837161a473901e8de2a0362031a51385a90
Amardeep Singh Yadav <ayadav4@global.sial.com>
2018-12-03 14:30:25 +0530

    initial commit

---
+++ 1/ec2_instance.tf
@@ -0,0 +1,19 @@
+### basic terraform script for ec2-instance
+
+provider "aws" {
+  access_key           = "AKIAISLZ3MBNU4XHRX3A"
+  secret_key           = "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"
+  region               = "us-east-2"
+}
+
+
+resource "aws_instance" "test" {
+  ami                  = "ami-0653e888ec96eab9b"
+  instance_type        = "t2.micro"
+}
+
+
+#########################################################
+##   ubuntu16.04 LTS   = ami-0653e888ec96eab9b         ##
+##   instance_type     = t2.micro                      ##
+#########################################################
