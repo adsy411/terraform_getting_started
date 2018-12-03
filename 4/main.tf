180eb69470dc16cf1cdb4e1f39ac6ca2dda97605
Amardeep Singh Yadav <ayadav4@global.sial.com>
2018-12-03 21:18:48 +0530

    setup_var

---
+++ 4/main.tf
@@ -0,0 +1,41 @@
+#############################################################
+###   In the step we are going to show defining variables   #
+###   here we defined regions variable those are defined in #
+###   "variables.tfvars"                                    #
+#############################################################
+variable "region"
+variable "shared_credentials_file"
+variable "profile"
+variable "my_ami" {
+  type = "map"
+}
+
+#############################################################
+#### Providers config                                       #
+#############################################################
+
+provider "aws" {
+  region                          = "${var.region}"
+  shared_credentials_file         = "${var.shared_credentials_file}"
+  profile                         = "${var.profile}"
+}
+#############################################################
+### Resource or in common term objectives                   #
+#############################################################
+resource "aws_instance" "test" {
+  ami = "${lookup(var.my_ami, var.region)}"
+  instance_type = "t2.micro"
+  tags {
+    Name = "test"
+  }
+}
+
+
+
+##############################################################
+### Steps to reproduce output                                #
+### $terraform init                                          #
+### $terraform plan                                          #
+### $terrafrom apply                                         #
+### $terraform destroy                                       #
+##############################################################
---
+++ 4/variables.tfvars
@@ -0,0 +1,18 @@
+#############################################################
+###   In the step we are going to show defining variables   #
+###   here we defined regions variable                      #
+#############################################################
+
+
+region = "us-east-1"
+
+
+
+shared_credentials_file = "/home/brightlab_team/.aws/credentials"
+
+
+
+my_ami = {
+  "us-east-1" = "ami-0f9cf087c1f27d9b1"
+  "us-east-2" = "ami-0653e888ec96eab9b"
+  }
